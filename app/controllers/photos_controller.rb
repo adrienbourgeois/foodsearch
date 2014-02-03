313d154c2a96c6d37eb7d76a176364c7696f2b89
Adrian <adrienfrancis.bourgeois@gmail.com>
2014-02-03 16:00:49 +1100

    assets precomp

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
 
   def index
-    render json: Photo.where(checked:true).to_json(include: :place)
+    render json: Photo.where(checked:true).limit(1000).to_json(include: :place)
   end
 
   def show
